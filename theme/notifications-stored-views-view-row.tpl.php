d22c57757b3cc49e39d672e839dd195b70a2a018
Makara Wang <makara15@gmail.com>
2011-08-01 14:25:18 +0800

    first commit

---
+++ theme/notifications-stored-views-view-row.tpl.php
@@ -0,0 +1,6 @@
+<?php if (isset($fields['title'])): ?>
+<h2><?php print $fields['title']; ?></h2>
+<?php endif; ?>
+<?php if (isset($fields['description'])): ?>
+<?php print $fields['description']; ?>
+<?php endif; ?>
---
+++ theme/notifications-stored-views-view-style.tpl.php
@@ -0,0 +1,1 @@
+<?php foreach ($rows as $id => $row): ?><?php print $row; ?><?php endforeach; ?>
