c8a403bf084a73ff82446d45f84e6a370913e654
Makara Wang <makara15@gmail.com>
2011-08-17 18:57:26 +0800

    move something to mn data

--- theme/notifications-stored-views-view-row.tpl.php
+++ theme/notifications-stored-views-view-row.tpl.php
@@ -1,6 +1,10 @@
 <?php if (isset($fields['title'])): ?>
 <h2><?php print $fields['title']; ?></h2>
 <?php endif; ?>
+<?php if (isset($fields['timestamp'])): ?>
+<p><?php print $fields['timestamp']; ?></p>
+<?php endif; ?>
+<hr>
 <?php if (isset($fields['description'])): ?>
-<?php print $fields['description']; ?>
+<p><?php print $fields['description']; ?></p>
 <?php endif; ?>
